47f6c349aa15bf98f69ef1d65b17aee143803be9
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-01-26 16:22:30 +0530

    updated to images of api and client

--- client/src/components/Common/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className='Footer'>
-        © Paper Genie - University of Technology and Applied science Muscat 2023
-    </div>
-  )
-}
--- client/src/components/baseURL.js
+++
@@ -1,5 +0,0 @@
-
-export const baseURL = 'http://localhost:4000'
-// export const baseURL = 'https://api.sset.co.in'
-
-export default baseURL;
